d1273a5da04d4650fc9b3dc9fbbcd83501e1898c
mEnOme <134943669+kulsumm@users.noreply.github.com>
2025-12-12 15:48:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -113,4 +113,6 @@ def predict():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    gunicorn app:app --bind 0.0.0.0:$PORT --workers 1
+
+
